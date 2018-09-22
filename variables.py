ec4abc91622570a6d989ea92f4d937be227ebc57
Vinay Singh <vinay@paymonk.com>
2018-09-22 15:46:08 +0530

    Variable Updates

--- variables.py
+++ variables.py
@@ -1,29 +1,27 @@
 """ Variable Tutorials """
 """
-   Variables are nothing but reserved memory locations to store values. 
-   This means that when you create a variable you reserve some space in memory
+	Variables are nothing but reserved memory locations to store values. 
+   	This means that when you create a variable you reserve some space in memory
 """
 
 
-"""
-	Basic Variable:
-	- one -> Variable Name
-	- 1   -> Variable Value
-"""
+""" Basic Variable """
 one = 1
 print(one) 
 """ Output : 1 """
 
 
-"""
-	Boolean Variable:
-	- true_variable -> Variable Name
-	- True 			-> Variable Value
-	Note:
-	- Value of boolean variable should be (True Or False) Not (true or false)
-"""
+"""	Boolean Variable """
 true_variable = True
 print(true_variable)
+""" Output : True """
+
+
+""" String Variable """
+
+
+
+
 
 
 
