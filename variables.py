f4c6d8c1694951463ae1e0101a035f377dbe4107
Vinay Singh <vinay@paymonk.com>
2018-09-22 15:40:22 +0530

    Variable Practice

---
+++ variables.py
@@ -0,0 +1,30 @@
+""" Variable Tutorials """
+"""
+   Variables are nothing but reserved memory locations to store values. 
+   This means that when you create a variable you reserve some space in memory
+"""
+
+
+"""
+	Basic Variable:
+	- one -> Variable Name
+	- 1   -> Variable Value
+"""
+one = 1
+print(one) 
+""" Output : 1 """
+
+
+"""
+	Boolean Variable:
+	- true_variable -> Variable Name
+	- True 			-> Variable Value
+	Note:
+	- Value of boolean variable should be (True Or False) Not (true or false)
+"""
+true_variable = True
+print(true_variable)
+
+
+
+
